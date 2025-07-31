6a5816da767d4db97d944b2c7f1c308f35d7dc20
lucashartmann <lucas.a111@hotmail.com>
2025-07-30 21:15:46 -0300

    Arumado: Buscar produtos no estoque

--- Código/src/view/TelaEstoque.py
+++ Código/src/view/TelaEstoque.py
@@ -21,107 +21,134 @@ class TelaEstoque(Screen):
         produtos_str = [str(produto) for produto in self.produtos]
         self.mount(Pretty(produtos_str))
 
-    def on_input_changed(self, evento):
-        texto = self.query_one(Input).value
+    def on_input_changed(self, evento: Input.Changed):
+        texto = evento.value
         resultado = self.query_one(Pretty)
         palavras = texto.split()
 
         if len(palavras) > 1:
+            self.produtos_filtrados = []
             if "marca:" in palavras:
                 index = palavras.index("marca:")
                 if index + 1 < len(palavras):
                     marca_busca = palavras[index + 1]
-                    if len(self.produtos_filtrados) > 0 and len(palavras) > 2:
-                        for produto1 in self.produtos_filtrados:
-                            if produto1.get_marca() != marca_busca:
-                                self.produtos_filtrados.remove(produto1)
+                    if len(self.produtos_filtrados) > 0:
+                        produtos_temp = []
+                        for produto in self.produtos_filtrados:
+                            if produto.get_marca() == marca_busca:
+                                produtos_temp.append(produto)
+                        self.produtos_filtrados = produtos_temp
                     else:
                         for produto in self.produtos:
-                            if produto.get_marca() == marca_busca and produto not in self.produtos_filtrados:
+                            if produto.get_marca() == marca_busca:
                                 self.produtos_filtrados.append(produto)
 
             if "valor:" in palavras:
                 index = palavras.index("valor:")
                 if index + 1 < len(palavras):
-                    valor_busca = float(palavras[index + 1])
-                    if len(self.produtos_filtrados) > 0 and len(palavras) > 2:
-                        for produto1 in self.produtos_filtrados:
-                            if produto1.get_preco() != valor_busca:
-                                self.produtos_filtrados.remove(produto1)
-                    else:
-                        for produto in self.produtos:
-                            if produto.get_preco() == valor_busca and produto not in self.produtos_filtrados:
-                                self.produtos_filtrados.append(produto)
+                    try:
+                        valor_busca = float(palavras[index + 1])
+                        if len(self.produtos_filtrados) > 0:
+                            produtos_temp = []
+                            for produto in self.produtos_filtrados:
+                                if produto.get_preco() == valor_busca:
+                                    produtos_temp.append(produto)
+                            self.produtos_filtrados = produtos_temp
+                        else:
+                            for produto in self.produtos:
+                                if produto.get_preco() == valor_busca:
+                                    self.produtos_filtrados.append(produto)
+                    except ValueError:
+                        self.notify("Valor inválido")
 
             if "nome:" in palavras:
                 index = palavras.index("nome:")
                 if index + 1 < len(palavras):
                     nome_busca = palavras[index + 1]
-                    if len(self.produtos_filtrados) > 0 and len(palavras) > 2:
-                        for produto1 in self.produtos_filtrados:
-                            if produto1.get_nome() != nome_busca:
-                                self.produtos_filtrados.remove(produto1)
+                    if len(self.produtos_filtrados) > 0:
+                        produtos_temp = []
+                        for produto in self.produtos_filtrados:
+                            if produto.get_nome() == nome_busca:
+                                produtos_temp.append(produto)
+                        self.produtos_filtrados = produtos_temp
                     else:
                         for produto in self.produtos:
-                            if produto.get_nome() == nome_busca and produto not in self.produtos_filtrados:
+                            if produto.get_nome() == nome_busca:
                                 self.produtos_filtrados.append(produto)
 
             if "quantidade:" in palavras:
                 index = palavras.index("quantidade:")
                 if index + 1 < len(palavras):
-                    quantidade_busca = int(palavras[index + 1])
-                    if len(self.produtos_filtrados) > 0 and len(palavras) > 2:
-                        for produto1 in self.produtos_filtrados:
-                            if produto1.get_quantidade() != quantidade_busca:
-                                self.produtos_filtrados.remove(produto1)
-                    else:
-                        for produto in self.produtos:
-                            if produto.get_quantidade() == quantidade_busca and produto not in self.produtos_filtrados:
-                                self.produtos_filtrados.append(produto)
+                    try:
+                        quantidade_busca = int(palavras[index + 1])
+                        if len(self.produtos_filtrados) > 0:
+                            produtos_temp = []
+                            for produto in self.produtos_filtrados:
+                                if produto.get_quantidade() == quantidade_busca:
+                                    produtos_temp.append(produto)
+                            self.produtos_filtrados = produtos_temp
+                        else:
+                            for produto in self.produtos:
+                                if produto.get_quantidade() == quantidade_busca:
+                                    self.produtos_filtrados.append(produto)
+                    except ValueError:
+                        self.notify("Valor inválido")
 
             if "modelo:" in palavras:
                 index = palavras.index("modelo:")
                 if index + 1 < len(palavras):
                     modelo_busca = palavras[index + 1].upper()
-                    if len(self.produtos_filtrados) > 0 and len(palavras) > 2:
-                        for produto1 in self.produtos_filtrados:
-                            if produto1.get_modelo() != modelo_busca:
-                                self.produtos_filtrados.remove(produto1)
+                    if len(self.produtos_filtrados) > 0:
+                        produtos_temp = []
+                        for produto in self.produtos_filtrados:
+                            if produto.get_modelo() == modelo_busca:
+                                produtos_temp.append(produto)
+                        self.produtos_filtrados = produtos_temp
                     else:
                         for produto in self.produtos:
-                            if produto.get_modelo() == modelo_busca and produto not in self.produtos_filtrados:
+                            if produto.get_modelo() == modelo_busca:
                                 self.produtos_filtrados.append(produto)
 
             if "cor:" in palavras:
                 index = palavras.index("cor:")
-                if index + 1 < len(palavras):
+                if index + 1 < len(palavras) and len(palavras[index + 1]) > 3:
                     cor_busca = palavras[index + 1]
-                    if len(self.produtos_filtrados) > 0 and len(palavras) > 2:
-                        for produto1 in self.produtos_filtrados:
-                            if produto1.get_cor() != cor_busca:
-                                self.produtos_filtrados.remove(produto1)
+                    if len(self.produtos_filtrados) > 0:
+                        produtos_temp = []
+                        for produto in self.produtos_filtrados:
+                            if produto.get_cor() == cor_busca:
+                                produtos_temp.append(produto)
+                        self.produtos_filtrados = produtos_temp
                     else:
                         for produto in self.produtos:
-                            if produto.get_cor() == cor_busca and produto not in self.produtos_filtrados:
+                            if produto.get_cor() == cor_busca:
                                 self.produtos_filtrados.append(produto)
 
             if "id:" in palavras:
                 index = palavras.index("id:")
                 if index + 1 < len(palavras):
-                    id_busca = int(palavras[index + 1])
-                    if len(self.produtos_filtrados) > 0 and len(palavras) > 2:
-                        for produto1 in self.produtos_filtrados:
-                            if produto1.get_id() != id_busca:
-                                self.produtos_filtrados.remove(produto1)
-                    else:
-                        for produto in self.produtos:
-                            if produto.get_id() == id_busca and produto not in self.produtos_filtrados:
-                                self.produtos_filtrados.append(produto)
-                                
+                    try:
+                        id_busca = int(palavras[index + 1])
+                        if len(self.produtos_filtrados) > 0:
+                            produtos_temp = []
+                            for produto in self.produtos_filtrados:
+                                if produto.get_id() == id_busca:
+                                    produtos_temp.append(produto)
+                            self.produtos_filtrados = produtos_temp
+                        else:
+                            for produto in self.produtos:
+                                if produto.get_id() == id_busca:
+                                    self.produtos_filtrados.append(produto)
+                    except ValueError:
+                        self.notify("Valor inválido")
+
             if len(self.produtos_filtrados) > 0:
                 produtos_str = [str(produto)
                                 for produto in self.produtos_filtrados]
                 resultado.update(produtos_str)
-            else: 
+            else:
                 produtos_str = [str(produto) for produto in self.produtos]
                 resultado.update(produtos_str)
+        else:
+            produtos_str = [str(produto) for produto in self.produtos]
+            resultado.update(produtos_str)
