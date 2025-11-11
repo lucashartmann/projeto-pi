ab8bcb1f8708a9d6faaf9eedc023abbb0f758e4c
lucashartmann <lucas.a111@hotmail.com>
2025-11-11 11:56:40 -0300

    +-

---
+++ Código/Desktop/model/Anuncio.py
@@ -0,0 +1,12 @@
+class Anuncio:
+    def __init__(self):
+        self.descricao = ""
+        self.titulo = ""
+        self.fotos = []
+        self.videos = []
+        self.anexos = []
+        self.corretor = ""
+        self.imovel = ""
+        self.proprietario = ""
+        self.captador = ""
+        
---
+++ Código/Desktop/model/Gerente.py
@@ -0,0 +1,20 @@
+class Gerente():
+    def __init__(self, nome, cpf, rg, telefone, endereco, email):
+        self.matricula = ""
+        self.salario = 0.0
+        self.cargo = ""
+        self.setor = ""
+        self.turno = ""
+        self.username = ""
+        self.senha = ""
+        self.email = email
+        self.telefones = []
+        self.id = 0
+        self.nome = nome
+        self.cpf = cpf
+        self.rg = rg
+        self.telefone = telefone
+        self.endereco = endereco
+        self.email = email
+        self.idade = 0
+        self.data_nascimento = ""
